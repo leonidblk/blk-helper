325db8da385eb9fb2d03f53a97827e7ca869da61
leonidblk <leonid@buildinglink.com>
2025-11-16 20:00:57 -0500

    move around section

--- PowershellCommands/Form1.Designer.cs
+++ PowershellCommands/Form1.Designer.cs
@@ -24,68 +24,86 @@ namespace PowershellCommands
 
         private void InitializeComponent()
         {
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.folderBrowserDialog3 = new System.Windows.Forms.FolderBrowserDialog();
-            this.vueOrchestratorFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
-            this.eventLogFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
-            this.eventLogVueFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
-            this.vueWebsiteCoreSection = new PowershellCommands.Controls.VueWebsiteCoreControl();
-            this.orchestratorSection = new PowershellCommands.Controls.OrchestratorPanelControl();
-            this.maintenanceApiSection = new PowershellCommands.Controls.MaintenanceApiPanelControl();
-            this.eventLogSection = new PowershellCommands.Controls.EventLogApiPanelControl();
-            this.eventLogVueSection = new PowershellCommands.Controls.EventLogVueControl();
-            this.SuspendLayout();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog3 = new FolderBrowserDialog();
+            vueOrchestratorFolderBrowserDialog = new FolderBrowserDialog();
+            eventLogFolderBrowserDialog = new FolderBrowserDialog();
+            eventLogVueFolderBrowserDialog = new FolderBrowserDialog();
+            vueWebsiteCoreSection = new PowershellCommands.Controls.VueWebsiteCoreControl();
+            orchestratorSection = new PowershellCommands.Controls.OrchestratorPanelControl();
+            maintenanceApiSection = new PowershellCommands.Controls.MaintenanceApiPanelControl();
+            eventLogSection = new PowershellCommands.Controls.EventLogApiPanelControl();
+            eventLogVueSection = new PowershellCommands.Controls.EventLogVueControl();
+            SuspendLayout();
             // 
             // vueWebsiteCoreSection
             // 
-            this.vueWebsiteCoreSection.Location = new System.Drawing.Point(30, 30);
-            this.vueWebsiteCoreSection.Name = "vueWebsiteCoreSection";
-            this.vueWebsiteCoreSection.Size = new System.Drawing.Size(703, 240);
-            this.vueWebsiteCoreSection.TabIndex = 0;
+            vueWebsiteCoreSection.BackColor = Color.WhiteSmoke;
+            vueWebsiteCoreSection.ConnectionStatusColor = Color.Red;
+            vueWebsiteCoreSection.ConnectionStatusText = "Connected to";
+            vueWebsiteCoreSection.Location = new Point(30, 30);
+            vueWebsiteCoreSection.Name = "vueWebsiteCoreSection";
+            vueWebsiteCoreSection.Size = new Size(703, 240);
+            vueWebsiteCoreSection.TabIndex = 0;
+            vueWebsiteCoreSection.VueCoreRootPath = "";
             // 
             // orchestratorSection
             // 
-            this.orchestratorSection.Location = new System.Drawing.Point(30, 300);
-            this.orchestratorSection.Name = "orchestratorSection";
-            this.orchestratorSection.Size = new System.Drawing.Size(703, 170);
-            this.orchestratorSection.TabIndex = 1;
+            orchestratorSection.BackColor = Color.WhiteSmoke;
+            orchestratorSection.Location = new Point(30, 276);
+            orchestratorSection.Name = "orchestratorSection";
+            orchestratorSection.OrchestratorPath = "";
+            orchestratorSection.Size = new Size(703, 170);
+            orchestratorSection.TabIndex = 1;
             // 
             // maintenanceApiSection
             // 
-            this.maintenanceApiSection.Location = new System.Drawing.Point(30, 480);
-            this.maintenanceApiSection.Name = "maintenanceApiSection";
-            this.maintenanceApiSection.Size = new System.Drawing.Size(703, 420);
-            this.maintenanceApiSection.TabIndex = 2;
-            this.maintenanceApiSection.Visible = false;
+            maintenanceApiSection.BackColor = Color.WhiteSmoke;
+            maintenanceApiSection.DatabaseConnectionStatusColor = Color.Red;
+            maintenanceApiSection.DatabaseConnectionStatusText = "Connected to...";
+            maintenanceApiSection.Location = new Point(30, 480);
+            maintenanceApiSection.MaintenanceRootPath = "";
+            maintenanceApiSection.MigrationName = "";
+            maintenanceApiSection.Name = "maintenanceApiSection";
+            maintenanceApiSection.Size = new Size(703, 371);
+            maintenanceApiSection.TabIndex = 2;
+            maintenanceApiSection.Visible = false;
             // 
             // eventLogSection
             // 
-            this.eventLogSection.Location = new System.Drawing.Point(30, 480);
-            this.eventLogSection.Name = "eventLogSection";
-            this.eventLogSection.Size = new System.Drawing.Size(703, 420);
-            this.eventLogSection.TabIndex = 3;
+            eventLogSection.BackColor = Color.WhiteSmoke;
+            eventLogSection.DatabaseConnectionStatusColor = Color.Red;
+            eventLogSection.DatabaseConnectionStatusText = "Connected to...";
+            eventLogSection.EventLogRootPath = "";
+            eventLogSection.Location = new Point(30, 452);
+            eventLogSection.MigrationName = "";
+            eventLogSection.Name = "eventLogSection";
+            eventLogSection.Size = new Size(703, 399);
+            eventLogSection.TabIndex = 3;
             // 
             // eventLogVueSection
             // 
-            this.eventLogVueSection.Location = new System.Drawing.Point(30, 930);
-            this.eventLogVueSection.Name = "eventLogVueSection";
-            this.eventLogVueSection.Size = new System.Drawing.Size(703, 180);
-            this.eventLogVueSection.TabIndex = 4;
+            eventLogVueSection.BackColor = Color.WhiteSmoke;
+            eventLogVueSection.Location = new Point(748, 30);
+            eventLogVueSection.Name = "eventLogVueSection";
+            eventLogVueSection.RootPath = "";
+            eventLogVueSection.Size = new Size(703, 180);
+            eventLogVueSection.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(766, 1140);
-            this.Controls.Add(this.eventLogVueSection);
-            this.Controls.Add(this.eventLogSection);
-            this.Controls.Add(this.maintenanceApiSection);
-            this.Controls.Add(this.orchestratorSection);
-            this.Controls.Add(this.vueWebsiteCoreSection);
-            this.Name = "Form1";
-            this.Text = "Blk Helper";
-            this.ResumeLayout(false);
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            ClientSize = new Size(1588, 869);
+            Controls.Add(eventLogVueSection);
+            Controls.Add(eventLogSection);
+            Controls.Add(maintenanceApiSection);
+            Controls.Add(orchestratorSection);
+            Controls.Add(vueWebsiteCoreSection);
+            Name = "Form1";
+            Text = "Blk Helper";
+            ResumeLayout(false);
         }
 
         #endregion
